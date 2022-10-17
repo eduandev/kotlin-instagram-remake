bd560cf7b07e8298e2acfb89f62d27cd2f295f0f
Andev <andremoraes930@gmail.com>
2022-10-17 17:48:04 -0300

    first commit

---
+++ app/src/main/java/edu/and/instagramramakeproject/login/view/LoginActivity.kt
@@ -0,0 +1,12 @@
+package edu.and.instagramramakeproject.login.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import edu.and.instagramramakeproject.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
